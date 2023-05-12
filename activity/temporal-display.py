79596790efb6665c5bd5bab8c090bcae38d55067
asreal089 <lexx_89@hotmail.com>
2023-05-12 16:28:43 -0300

    Show covid impact on B3

---
+++ activity/temporal-display.py
@@ -0,0 +1,24 @@
+from pandas import read_csv
+from matplotlib import pyplot as plt
+
+data = read_csv('b3_stocks_1994_2020.csv')
+filtered_data = data[data['ticker'] == 'IBOV11']
+filtered_data = filtered_data[filtered_data['datetime'] >= '2019-01-01']
+datetime = filtered_data['datetime']
+close_price = filtered_data['close']
+
+plt.figure(figsize=(10, 6))
+plt.plot(datetime, close_price, label='Close price BRL')
+
+# Customizing the chart
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title('IBOV11')
+plt.xticks(rotation=45, ha='right')
+plt.yticks(rotation=45, ha='right')
+plt.subplots_adjust(bottom=0.15)
+plt.legend()
+
+# Displaying the chart
+plt.tight_layout()
+plt.show()
