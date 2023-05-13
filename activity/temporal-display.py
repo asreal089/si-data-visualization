874c8a8767b5adc8a9b77ce217bf5cf61344524d
asreal089 <lexx_89@hotmail.com>
2023-05-13 12:21:41 -0300

    Add grap of volume X price comparisson

--- activity/temporal-display.py
+++ activity/temporal-display.py
@@ -26,7 +26,6 @@ plt.legend()
 # Displaying the chart
 plt.tight_layout()
 plt.show()
-
 plt.savefig('ibov11-covid-impact.png');
 
 
@@ -49,4 +48,30 @@ plt.ylabel('Volume')
 plt.title('IBOV11 volume');
 plt.xticks(rotation=45, ha='right');
 plt.show()
+plt.savefig('ibov11-volume.png');
 
+
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('Date')
+ax1.set_ylabel('Close Price', color=color)
+ax1.plot(datetime_covid_impact, close_price_covid_impact, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+plt.setp(ax1.xaxis.get_majorticklabels(), rotation=45)
+
+
+ax2 = ax1.twinx()
+color = 'tab:blue'
+ax2.set_ylabel('Volume', color=color)
+ax2.bar(datetime_covid_impact, volume_covid_impact, color=color, alpha=0.3)
+ax2.tick_params(axis='y', labelcolor=color)
+axes = plt.gca()
+axes.xaxis.set_major_locator(plt.MaxNLocator(nbins=11))
+
+# Rotate date labels automatically
+plt.xticks(rotation=90)
+
+fig.tight_layout()
+plt.show()
+plt.savefig('ibov11-volume-v-price-covid-impact.png');
