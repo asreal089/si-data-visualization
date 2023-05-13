2e92f32c87c44878978e0b3ba32e577bc46222c0
asreal089 <lexx_89@hotmail.com>
2023-05-12 22:48:48 -0300

    Add readme

---
+++ activity/Readme.md
@@ -0,0 +1,10 @@
+## Script to show data analisys
+
+how to run
+Inside this dir run the following:
+
+````
+python temporal-dsiplay.py
+````
+
+this script show the graph
--- activity/temporal-display.py
+++ activity/temporal-display.py
@@ -3,9 +3,9 @@ from matplotlib import pyplot as plt
 
 data = read_csv('b3_stocks_1994_2020.csv')
 filtered_data = data[data['ticker'] == 'IBOV11']
-filtered_data = filtered_data[filtered_data['datetime'] >= '2019-01-01']
-datetime = filtered_data['datetime']
-close_price = filtered_data['close']
+filtered_data_covid_impact = filtered_data[filtered_data['datetime'] >= '2019-01-01']
+datetime = filtered_data_covid_impact['datetime']
+close_price = filtered_data_covid_impact['close']
 
 plt.figure(figsize=(10, 6))
 plt.plot(datetime, close_price, label='Close price BRL')
