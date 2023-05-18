3932f1106ec98cd562ede68b6030110144d41351
asreal089 <lexx_89@hotmail.com>
2023-05-18 07:06:59 -0300

    adding vaccination ratio to chart

--- covid-world-analises/analisys.py
+++ covid-world-analises/analisys.py
@@ -5,6 +5,7 @@ data = read_csv('covid-vaccination-vs-death_ratio.csv')
 filtered_data_brazil = data[data['iso_code'] == 'BRA']
 filtered_data_brazil_date = filtered_data_brazil['date']
 filtered_data_brazil_new_death = filtered_data_brazil['New_deaths']
+filtered_data_brazil_vacination = filtered_data_brazil['ratio']
 
 plt.figure(figsize=(10, 6))
 plt.plot(filtered_data_brazil_date, filtered_data_brazil_new_death, label='new Covid-19 deaths in Brazil')
@@ -17,6 +18,12 @@ plt.xticks(rotation=45, ha='right')
 plt.yticks(rotation=45, ha='right')
 plt.subplots_adjust(bottom=0.15)
 plt.legend()
+
+ax2 = plt.twinx()
+color = 'tab:blue'
+ax2.set_ylabel('Vaccination ratio in population', color=color)
+ax2.bar(filtered_data_brazil_date, filtered_data_brazil_vacination, color=color, alpha=0.3)
+ax2.tick_params(axis='y', labelcolor=color)
 axes = plt.gca()
 axes.xaxis.set_major_locator(plt.MaxNLocator(nbins=11))
 
