d4a32e91e99d482a34758cde0a6ea4f08f9d54d2
asreal089 <lexx_89@hotmail.com>
2023-05-20 22:36:06 -0300

    Complete readme

---
+++ covid-world-analysis/README.md
@@ -0,0 +1,27 @@
+# Covid Deat vs Vaccination analisys
+
+Data set from Kaggle: `https://www.kaggle.com/datasets/sinakaraji/covid-vaccination-vs-death`
+
+Analisys made do display effects of vaccination.
+
+## How to run:
+
+it might be necessary install with pip
+
+```
+pip install plotly
+pip install pandas
+pip install matplotlib
+```
+
+Open terminal in this directory Run :
+```
+python analysis.py
+```
+Expected outcome:
+
+<img align="center" width="400px" height="250px" src="https://raw.githubusercontent.com/asreal089/si-data-visualization/main/covid-world-analysis/most_deaths_until_2022.png"/>
+
+<img align="center" width="400px" height="250px" src="https://raw.githubusercontent.com/asreal089/si-data-visualization/main/covid-world-analysis/brazil_new_deaths_vs_vacination.png"/>
+
+<img align="center" width="400px" height="250px" src="https://raw.githubusercontent.com/asreal089/si-data-visualization/main/covid-world-analysis/world_scatter_geo_plot.png"/>
---
+++ covid-world-analysis/analysis.py
@@ -0,0 +1,58 @@
+from pandas import read_csv
+from matplotlib import axes, pyplot as plt
+
+import plotly.express as px
+
+data = read_csv('covid-vaccination-vs-death_ratio.csv')
+
+# Consoling the data
+consolidated_data_iso_and_deaths = data[['iso_code','New_deaths', 'country']]
+consolidated_data = consolidated_data_iso_and_deaths.groupby(['iso_code', 'country'], as_index=False).sum()
+
+# sorting the data by deaths
+consolidated_data_sorted = consolidated_data.sort_values(by=['New_deaths'], ascending=False)
+filtered_most_deaths = consolidated_data_sorted.head(10)
+
+# bar chart most deaths
+plt.figure(figsize=(10, 6))
+plt.bar(filtered_most_deaths['country'], filtered_most_deaths['New_deaths'], label='Covid-19 deaths by country', color='red')
+plt.xticks(rotation=45, ha='right')
+plt.yticks(rotation=45, ha='right')
+plt.subplots_adjust(bottom=0.15)
+plt.show()
+
+filtered_data_brazil = data[data['iso_code'] == 'BRA']
+filtered_data_brazil_date = filtered_data_brazil['date']
+filtered_data_brazil_new_death = filtered_data_brazil['New_deaths']
+filtered_data_brazil_vacination = filtered_data_brazil['ratio']
+
+plt.figure(figsize=(10, 6))
+plt.plot(filtered_data_brazil_date, filtered_data_brazil_new_death, label='new Covid-19 deaths in Brazil', color='red')
+
+# Customizing the chart
+plt.xlabel('Date')
+plt.ylabel('Deaths')
+plt.title('Covid-19 deaths in Brazil')
+plt.xticks(rotation=45, ha='right')
+plt.yticks(rotation=45, ha='right')
+plt.subplots_adjust(bottom=0.15)
+plt.legend()
+
+ax2 = plt.twinx()
+color = 'tab:blue'
+ax2.set_ylabel('Vaccination ratio in population', color=color)
+ax2.bar(filtered_data_brazil_date, filtered_data_brazil_vacination, color=color, alpha=0.3)
+ax2.tick_params(axis='y', labelcolor=color)
+axes = plt.gca()
+axes.xaxis.set_major_locator(plt.MaxNLocator(nbins=11))
+
+# Displaying the chart
+plt.tight_layout()
+plt.show()
+
+# Consoling the data
+consolidated_data_iso_and_deaths = data[['iso_code','New_deaths', 'country']]
+consolidated_data = consolidated_data_iso_and_deaths.groupby(['iso_code', 'country'], as_index=False).sum()
+
+fig = px.scatter_geo(consolidated_data, locations="iso_code", color="country", hover_name="country", size="New_deaths", projection="natural earth")
+fig.show()
