ac624e90d4c7b4488a3d3bd8b545d7ac4a8ca4c0
asreal089 <lexx_89@hotmail.com>
2023-05-20 21:29:25 -0300

    Adding barchart for country with most deaths

--- covid-world-analises/analysis.py
+++ covid-world-analises/analysis.py
@@ -4,6 +4,23 @@ from matplotlib import axes, pyplot as plt
 import plotly.express as px
 
 data = read_csv('covid-vaccination-vs-death_ratio.csv')
+
+# Consoling the data
+consolidated_data_iso_and_deaths = data[['iso_code','New_deaths', 'country']]
+consolidated_data = consolidated_data_iso_and_deaths.groupby(['iso_code', 'country'], as_index=False).sum()
+
+# sorting the data by deaths
+consolidated_data_sorted = consolidated_data.sort_values(by=['New_deaths'], ascending=False)
+filtered_most_deaths = consolidated_data_sorted.head(10)
+
+# bar chart most deaths
+plt.figure(figsize=(10, 6))
+plt.bar(filtered_most_deaths['country'], filtered_most_deaths['New_deaths'], label='Covid-19 deaths', color='red')
+plt.xticks(rotation=45, ha='right')
+plt.yticks(rotation=45, ha='right')
+plt.subplots_adjust(bottom=0.15)
+plt.show()
+
 filtered_data_brazil = data[data['iso_code'] == 'BRA']
 filtered_data_brazil_date = filtered_data_brazil['date']
 filtered_data_brazil_new_death = filtered_data_brazil['New_deaths']
@@ -33,13 +50,9 @@ axes.xaxis.set_major_locator(plt.MaxNLocator(nbins=11))
 plt.tight_layout()
 plt.show()
 
-#geo scatter chart
-
 # Consoling the data
 consolidated_data_iso_and_deaths = data[['iso_code','New_deaths', 'country']]
-
-consolidated_data = consolidated_data_iso_and_deaths.groupby(['iso_code', 'country'],as_index=False).sum()
-
+consolidated_data = consolidated_data_iso_and_deaths.groupby(['iso_code', 'country'], as_index=False).sum()
 
 fig = px.scatter_geo(consolidated_data, locations="iso_code", color="country", hover_name="country", size="New_deaths", projection="natural earth")
 fig.show()
